82abb7e2abed27a1cc4c633f891db0f526348d1e
Navneet Singh <mufc.navneet@gmail.com>
2023-08-18 23:20:20 +0530

    inputs

---
+++ index.js
@@ -0,0 +1,4 @@
+var billAmount = document.querySelector("#bill");
+var cashAmount = document.querySelector("#cash");
+var btnCheck = document.querySelector("btn-check");
+var errorMessage = document.querySelector("#error");
