6738194b1bd0d9135480f531523fffc92a4f268c
Navneet Singh <mufc.navneet@gmail.com>
2023-08-20 17:34:50 +0530

    conversion functionality done

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-var billAmount = document.querySelector("#bill");
-var cashGiven = document.querySelector("#cash");
-var btnCheck = document.querySelector("#btn-check");
-var errorMessage = document.querySelector("#error");
+const billAmount = document.querySelector('#bill');
+const cashGiven = document.querySelector('#cash');
+const btnCheck = document.querySelector('#btn-check');
+const errorMessage = document.querySelector('#error');
+const noOfNotes = document.querySelectorAll("#no-of-notes");
+const notesArray = [2000, 500, 100, 20, 10, 5, 1];
 
-function validation(){
-    errorMessage.style.display = "none";
-    if(billAmount.value >= 0){
 
+function validation() {
+    hideMessage();
+
+
+    const billAmountValue = parseFloat(billAmount.value);
+    const cashGivenValue = parseFloat(cashGiven.value);
+
+    if (billAmountValue > 0) {
+        if (cashGivenValue >= billAmountValue) {
+            const returnAmount = cashGivenValue - billAmountValue;
+            calculateChange(returnAmount);
+        }
+        else {
+            message("Do you wanna wash plates?");
+        }
     }
-    else
-        errorMessage.style.display = "block";
-        errorMessage.innerHTML = "Invalid amount veere";
-    
+    else {
+        message("Invalid amount entered");
+    }
 
 }
 
+function calculateChange(returnAmount) {
 
-btnCheck.addEventListener("click", validation);
+    for (let i = 0; i < notesArray.length; i++) {
+        const currentChange = Math.trunc(returnAmount / notesArray[i]);
+        returnAmount %= notesArray[i];
+        noOfNotes[i].innerText = currentChange;
+
+    }
+
+}
+
+function hideMessage() {
+
+    errorMessage.style.display = "none";
+}
+
+function message(msg) {
+
+    errorMessage.style.display = "block";
+    errorMessage.innerText = msg;
+}
+
+btnCheck.addEventListener('click', validation);
