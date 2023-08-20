1035534d334034034850385bd9dcd5e6f0be38df
Navneet Singh <mufc.navneet@gmail.com>
2023-08-20 16:01:13 +0530

    refactor

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var btnCheck = document.querySelector("#btn-check");
 var errorMessage = document.querySelector("#error");
 
 function validation(){
-    
+    errorMessage.style.display = "none";
     if(billAmount.value >= 0){
 
     }
     else
+        errorMessage.style.display = "block";
         errorMessage.innerHTML = "Invalid amount veere";
     
 
